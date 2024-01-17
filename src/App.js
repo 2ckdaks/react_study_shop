4a83c959e79dd936f2d9f26680c2c1562fabd287
changmin <changmin@digital-x.kr>
2024-01-17 13:13:56 +0900

    router이용 페이지 이동

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import data from './data.js';
 import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   let [shoes, setShoes] = useState(data);
@@ -12,22 +13,33 @@ function App() {
     <div className='App'>
       <Navbar bg='dark' variant='dark'>
         <Container>
-          <Navbar.Brand href='#home'>Navbar</Navbar.Brand>
+          <Navbar.Brand href='/'>Navbar</Navbar.Brand>
           <Nav className='me-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#features'>Features</Nav.Link>
-            <Nav.Link href='#pricing'>Pricing</Nav.Link>
+            <Nav.Link href='/about'>About</Nav.Link>
+            <Nav.Link href='/features'>Features</Nav.Link>
+            <Nav.Link href='/pricing'>Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className='main-bg'>{/* <img src='/bg.png' /> */}</div>
-      <div className='container'>
-        <div className='row'>
-          {shoes.map(function (a, i) {
-            return <Card shoes={shoes[i]} i={i + 1} key={i} />;
-          })}
-        </div>
-      </div>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <div>
+              <div className='main-bg'>{/* <img src='/bg.png' /> */}</div>
+              <div className='container'>
+                <div className='row'>
+                  {shoes.map(function (a, i) {
+                    return <Card shoes={shoes[i]} i={i + 1} key={i} />;
+                  })}
+                </div>
+              </div>
+            </div>
+          }
+        />
+        <Route path='/about' element={<div>어바웃페이지임</div>} />
+        <Route path='/detail' element={<div>상세페이지임</div>} />
+      </Routes>
     </div>
   );
 }
