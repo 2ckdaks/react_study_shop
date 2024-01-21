c4e4fe52dbb41b1e1fb452109ed606e7411c7149
이창민 <2ckdaks@gmail.com>
2024-01-21 15:27:01 +0900

    탭 UI제작

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 
 let Box = styled.div`
   padding: 20px;
@@ -15,6 +16,7 @@ let YellowBtn = styled.button`
 export default function Detail(props) {
   let { id } = useParams();
   let [alert, setAlert] = useState(true);
+  let [탭, 탭변경] = useState(0);
 
   useEffect(() => {
     //여기적은 코드는 컴포넌트 로드 & 업데이트 마다 실행됨
@@ -27,26 +29,71 @@ export default function Detail(props) {
   }, []);
 
   return (
-    <div className='container'>
-      <Box className='row'>
-        <div className='col-md-6'>
+    <div className="container">
+      <Box className="row">
+        <div className="col-md-6">
           <img
-            src='https://codingapple1.github.io/shop/shoes1.jpg'
-            width='100%'
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
           />
         </div>
-        <div className='col-md-6'>
-          <h4 className='pt-5'>{props.shoes[id].title}</h4>
+        <div className="col-md-6">
+          <h4 className="pt-5">{props.shoes[id].title}</h4>
           <p>{props.shoes[id].content}</p>
           <p>{props.shoes[id].price}</p>
-          <YellowBtn bg='skyblue' className='btn btn-danger'>
+          <YellowBtn bg="skyblue" className="btn btn-danger">
             주문하기
           </YellowBtn>
         </div>
       </Box>
       {alert == true ? (
-        <div className='alert alert-warning'>2초이내 구매시 할인</div>
+        <div className="alert alert-warning">2초이내 구매시 할인</div>
       ) : null}
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => {
+              탭변경(0);
+            }}
+            eventKey="link0"
+          >
+            버튼0
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => {
+              탭변경(1);
+            }}
+            eventKey="link1"
+          >
+            버튼1
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => {
+              탭변경(2);
+            }}
+            eventKey="link2"
+          >
+            버튼2
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TabContent 탭={탭} />
     </div>
   );
+}
+
+function TabContent(props) {
+  if (props.탭 === 0) {
+    return <div>내용0</div>;
+  }
+  if (props.탭 === 1) {
+    return <div>내용1</div>;
+  }
+  if (props.탭 === 2) {
+    return <div>내용2</div>;
+  }
 }
