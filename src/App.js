f3f8d00e01b3e4bd4ed5540be5599fd45348157c
이창민 <2ckdaks@gmail.com>
2024-01-24 00:16:28 +0900

    lazy import 학습

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import data from "./data.js";
-import { createContext, useEffect, useState } from "react";
+import { createContext, lazy, Suspense, useEffect, useState } from "react";
 import {
   Routes,
   Route,
@@ -12,9 +12,13 @@ import {
   json,
 } from "react-router-dom";
 import axios from "axios";
-import Detail from "./routes/Detail.js";
-import Cart from "./routes/Cart";
+// import Detail from "./routes/Detail.js";
+// import Cart from "./routes/Cart";
 import { useQuery } from "react-query";
+
+//메인페이지에서 보이지않는 컴포넌트를 굳이 바로 import할필요없어 lazy하게 임포트
+const Detail = lazy(() => import("./routes/Detail.js"));
+const Cart = lazy(() => import("./routes/Cart.js"));
 
 // export let Context1 = React.createContext();
 export let Context1 = createContext();
@@ -53,67 +57,69 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <div className="main-bg">{/* <img src='/bg.png' /> */}</div>
-              <div className="container">
-                <div className="row">
-                  {shoes.map(function (a, i) {
-                    return <Card shoes={shoes[i]} i={i + 1} key={i} />;
-                  })}
+      <Suspense fallback={<div>loading</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <div className="main-bg">{/* <img src='/bg.png' /> */}</div>
+                <div className="container">
+                  <div className="row">
+                    {shoes.map(function (a, i) {
+                      return <Card shoes={shoes[i]} i={i + 1} key={i} />;
+                    })}
+                  </div>
                 </div>
-              </div>
-              <button
-                onClick={() => {
-                  axios
-                    .get("https://codingapple1.github.io/shop/data2.json")
-                    .then((결과) => {
-                      console.log(결과.data);
-                      let copy = [...shoes];
-                      copy = [...shoes, ...결과.data];
-                      setShoes(copy);
-                    })
-                    .catch(() => {
-                      console.log("실패함");
-                    });
-                }}
-              >
-                get요청 버튼
-              </button>
-              {/* <button onClick={()=>{
+                <button
+                  onClick={() => {
+                    axios
+                      .get("https://codingapple1.github.io/shop/data2.json")
+                      .then((결과) => {
+                        console.log(결과.data);
+                        let copy = [...shoes];
+                        copy = [...shoes, ...결과.data];
+                        setShoes(copy);
+                      })
+                      .catch(() => {
+                        console.log("실패함");
+                      });
+                  }}
+                >
+                  get요청 버튼
+                </button>
+                {/* <button onClick={()=>{
                 axios.post(
                   'api', {name : 'kim'}
                   )
               }}>post요청 버튼</button> */}
-            </div>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <div>
-              어바웃페이지임<Outlet></Outlet>
-            </div>
-          }
-        >
-          <Route path="member" element={<div>멤버들</div>} />
-          <Route path="location" element={<div>회사위치</div>} />
-        </Route>
+              </div>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <div>
+                어바웃페이지임<Outlet></Outlet>
+              </div>
+            }
+          >
+            <Route path="member" element={<div>멤버들</div>} />
+            <Route path="location" element={<div>회사위치</div>} />
+          </Route>
 
-        <Route
-          path="/detail/:id"
-          element={
-            <Context1.Provider value={{ 재고, shoes }}>
-              <Detail shoes={shoes} />
-            </Context1.Provider>
-          }
-        />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="*" element={<div>없는페이지임</div>} />
-      </Routes>
+          <Route
+            path="/detail/:id"
+            element={
+              <Context1.Provider value={{ 재고, shoes }}>
+                <Detail shoes={shoes} />
+              </Context1.Provider>
+            }
+          />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<div>없는페이지임</div>} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
